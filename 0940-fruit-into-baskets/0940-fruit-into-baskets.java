7f5f6c045d11104a57167102fed8836e604a8912
Mansi Bhandari <148347130+Thejaggeddevil@users.noreply.github.com>
2025-08-04 11:00:29 +0530

    Time: 50 ms (59.78%), Space: 55.3 MB (40.84%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,28 @@
+
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> basket = new HashMap<>();
+        int left = 0, maxFruits = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            int fruit = fruits[right];
+            basket.put(fruit, basket.getOrDefault(fruit, 0) + 1);
+
+            // If we have more than 2 types, shrink the window
+            while (basket.size() > 2) {
+                int leftFruit = fruits[left];
+                basket.put(leftFruit, basket.get(leftFruit) - 1);
+                if (basket.get(leftFruit) == 0) {
+                    basket.remove(leftFruit);
+                }
+                left++;
+            }
+
+            // Update max fruits collected
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        return maxFruits;
+    }
+}
