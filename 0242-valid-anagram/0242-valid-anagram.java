ff20b1ec12f368ded53e3c7d31fee0d7e7d48eb6
Mansi Bhandari <148347130+Thejaggeddevil@users.noreply.github.com>
2026-02-02 06:48:57 +0530

    Time: 16 ms (21.71%), Space: 46.6 MB (31.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-         char[]sSort = s.toCharArray();
-         char[]tSort =t.toCharArray();
-         Arrays.sort(sSort);
-         Arrays.sort(tSort);
-         return Arrays.equals(sSort,tSort);
+         if(s.length()!=t.length())return false;
+        //  char[]sSort = s.toCharArray();
+        //  char[]tSort =t.toCharArray();
+        //  Arrays.sort(sSort);
+        //  Arrays.sort(tSort);
+        //  return Arrays.equals(sSort,tSort);
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for(char c : t.toCharArray()){
+            if(!map.containsKey(c))return false;
+            map.put(c,map.get(c)-1);
+            if(map.get(c)<0)return false;
+
+        }
+return true;
         
     }
 }
